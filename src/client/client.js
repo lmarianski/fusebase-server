78e2f360a9e4a3a50a6f59fbc85979f898403654
lukas2005 <lukas2005.38@gmail.com>
2018-05-05 20:23:28 +0200

    Update client.js

--- src/client/client.js
+++ src/client/client.js
@@ -94,6 +94,6 @@ function client() {
 	// on reconnection, reset the transports option, as the Websocket
 	// connection may have failed (caused by proxy, firewall, browser, ...)
 	socket.on('reconnect_attempt', () => {
-		//socket.io.opts.transports = ['polling', 'websocket'];
+		socket.io.opts.transports = ['polling', 'websocket'];
 	});
 }
